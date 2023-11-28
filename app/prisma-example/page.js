1c29488b43d6872a307470ab97cf6139ad298f63
Pedro Dias <pedromndias@gmail.com>
2023-11-28 10:40:11 +0100

    created routes

---
+++ app/prisma-example/page.js
@@ -0,0 +1,9 @@
+const PrismaExamplePage = () => {
+    return (
+      <div>
+        <h1 className="text-7xl">PrismaExample Page</h1>
+      </div>
+    )
+  }
+  
+  export default PrismaExamplePage
