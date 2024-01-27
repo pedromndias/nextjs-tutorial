178d8c32c3896b78a5b0b7dcd2545631fbc917df
Pedro Dias <pedromndias@gmail.com>
2024-01-27 23:29:27 +0100

    server actions code refactor

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,19 +1,4 @@
-import prisma from "@/utils/db"
-import {revalidatePath} from "next/cache"
-
-// ? A server action is an asynchronous function that is executed on the server. It handles form submissions and data mutations. Note the formData which is a web api interface that comes already with Nextjs. Also note the "use server", as this function will run in the server.
-const createTask = async (formData) => {
-  "use server"
-  const content = formData.get("content")
-  // console.log(content);
-  await prisma.task.create({
-    data: {
-      content,
-    }
-  })
-  // Note that we need to revalidate the path because the component is static and otherwise it would not reflect the updates:
-  revalidatePath("/tasks")
-}
+import { createTask } from "@/utils/actions"
 
 const TaskForm = () => {
   return <form action={createTask}>
---
+++ utils/actions.js
@@ -0,0 +1,29 @@
+// ? A server action is an asynchronous function that is executed on the server. It handles form submissions and data mutations. 
+
+"use server"
+
+import prisma from "@/utils/db"
+import {revalidatePath} from "next/cache"
+
+export const getAllTasks = async () => {
+    return await prisma.task.findMany({
+        orderBy: {
+            createdAt: "desc"
+        }
+    })
+}
+
+
+// ? Note the formData which is a web api interface that comes already with Nextjs. Also note the "use server", as this function will run in the server.
+
+export const createTask = async(formData) => {
+    const content = formData.get("content")
+    // console.log(content);
+    await prisma.task.create({
+        data: {
+            content,
+        }
+    })
+    // Note that we need to revalidate the path because the component is static and otherwise it would not reflect the updates:
+    revalidatePath("/tasks")
+}
